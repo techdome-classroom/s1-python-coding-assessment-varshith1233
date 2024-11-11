52102c38da59752d3c2564899ff7f990cd701bd1
Charan P <charanp@Charans-MacBook-Pro.local>
2024-11-11 14:04:17 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,3 +4,31 @@ class Solution:
     #    write your code here
                     
         return 0
+
+class Solution:
+    
+    def countIslands(self, map: list[list[str]]) -> int:
+        if not map or len(map) == 0:
+            return 0
+
+        num_islands = 0
+
+        def markVisited(x: int, y: int):
+            if x < 0 or y < 0 or x >= len(map) or y >= len(map[0]) or map[x][y] == 'W':
+                return
+            
+            map[x][y] = 'W'
+
+            markVisited(x + 1, y)
+            markVisited(x - 1, y)
+            markVisited(x, y + 1)
+            markVisited(x, y - 1)
+
+        for i in range(len(map)):
+            for j in range(len(map[0])):
+                if map[i][j] == 'L':
+                    num_islands += 1
+                    markVisited(i, j)
+
+        return num_islands
+
